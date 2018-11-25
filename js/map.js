c61305cdb81e595a28f4d7beb168aec85acd950e
DmitriyS87 <schukindmitriy@gmail.com>
2018-11-25 14:01:06 +0800

    изменили названия и константы

--- js/map.js
+++ js/map.js
@@ -11,15 +11,14 @@ var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
 var NUMBER_ESTATE_OBJECTS = 8;
 var DEFAULT_CARD_INDEX = 0;
+var ESTATE_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var ESTATE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECK_IN_OUT_VARIANTS = ['12:00', '13:00', '14:00'];
+var FEATURES_VARIANTS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-var EstateTitles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var EstateTypes = ['palace', 'flat', 'house', 'bungalo'];
-var CheckInVariants = ['12:00', '13:00', '14:00'];
-var CheckOutVariants = ['12:00', '13:00', '14:00'];
-var FeaturesVariants = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var EstatePhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var getMassiveSequence = function (max) {
+var getArraySequence = function (max) {
   var sequence = [];
   for (var index = 0; index < max; index++) {
     sequence[index] = index;
@@ -27,24 +26,24 @@ var getMassiveSequence = function (max) {
   return sequence;
 };
 
-var getMassiveRandomSequence = function (massive) {
-  var sequence = getMassiveSequence(massive.length);
+var getArrayRandomSequence = function (massive) {
+  var sequence = getArraySequence(massive.length);
   var randomIndex;
-  var auxiliarySequence = getMassiveSequence(massive.length);
+  var auxiliarySequence = getArraySequence(massive.length);
   for (var j = 0; j < massive.length; j++) {
     randomIndex = getRandomMinMax(0, massive.length - 1 - j);
-    sequence[j] = auxiliarySequence[randomIndex];
+    sequence.push(auxiliarySequence[randomIndex]);
     auxiliarySequence.splice(randomIndex, 1);
   }
   return sequence;
 
 };
 
-var getMassivePhotos = function (massivePhotos) {
-  var randomSequence = getMassiveRandomSequence(massivePhotos);
+var getArrayPhotos = function (massivePhotos) {
+  var randomSequence = getArrayRandomSequence(massivePhotos);
   var photosString = [];
   for (var j = 0; j < massivePhotos.length; j++) {
-    photosString[j] = EstatePhotos[randomSequence[j]];
+    photosString.push(EstatePhotos[randomSequence[j]]);
   }
   return photosString;
 
@@ -59,10 +58,10 @@ var getLocationXOrY = function (min, max) {
   return getRandomMinMax(min, max);
 };
 
-var getOneStringFromMassive = function (massive, endItem) {
+var cropArray = function (massive, endItem) {
   var outputMassive = [];
   for (var i = 0; i < endItem; i++) {
-    outputMassive[i] = massive[i];
+    outputMassive.push(massive[i]);
   }
   return outputMassive;
 };
@@ -76,17 +75,17 @@ var getRandomEstateObject = function (numberEstateOblect) {
     author: {
       avatar: AVATARS_PATH + (avatarsRandomSequenceIndex[numberEstateOblect] + 1) + '.png'},
     offer: {
-      title: EstateTitles[estateTitlesIndex[numberEstateOblect]],
+      title: ESTATE_TITLES[estateTitlesIndex[numberEstateOblect]],
       address: [locationX, locationY],
       price: getRandomMinMax(MIN_PRICE_UNIT, MAX_PRICE_UNIT),
-      type: EstateTypes[getRandomMinMax(0, EstateTypes.length - 1)],
+      type: ESTATE_TYPES[getRandomMinMax(0, ESTATE_TYPES.length - 1)],
       rooms: countRooms,
       guests: getRandomMinMax(1, (countRooms * MAX_GUESTS_IN_ROOMS)),
-      checkin: CheckInVariants[getRandomMinMax(0, CheckInVariants.length - 1)],
-      checkout: CheckOutVariants[getRandomMinMax(0, CheckOutVariants.length - 1)],
-      features: getOneStringFromMassive(FeaturesVariants, getRandomMinMax(0, FeaturesVariants.length)),
+      checkin: CHECK_IN_OUT_VARIANTS[getRandomMinMax(0, CHECK_IN_OUT_VARIANTS.length - 1)],
+      checkout: CHECK_IN_OUT_VARIANTS[getRandomMinMax(0, CHECK_IN_OUT_VARIANTS.length - 1)],
+      features: cropArray(FEATURES_VARIANTS, getRandomMinMax(0, FEATURES_VARIANTS.length)),
       description: '',
-      photos: getMassivePhotos(EstatePhotos)
+      photos: getArrayPhotos(EstatePhotos)
     },
 
     location: {
@@ -167,9 +166,9 @@ var pushCardData = function (currentObject) {
   return newCard;
 };
 
-var avatarsSequenceIndex = getMassiveSequence(AUTHORS_COUNT);
-var avatarsRandomSequenceIndex = getMassiveRandomSequence(avatarsSequenceIndex);
-var estateTitlesIndex = getMassiveRandomSequence(EstateTitles);
+var avatarsSequenceIndex = getArraySequence(AUTHORS_COUNT);
+var avatarsRandomSequenceIndex = getArrayRandomSequence(avatarsSequenceIndex);
+var estateTitlesIndex = getArrayRandomSequence(ESTATE_TITLES);
 
 var estateObjects = [];
 
@@ -181,7 +180,7 @@ var mapMaxX = insertPlacePin.clientWidth - parseInt(pinTemplateWidth, 10);
 var mapMinX = 0 + parseInt(pinTemplateWidth, 10);
 
 for (var i = 0; i < NUMBER_ESTATE_OBJECTS; i++) {
-  estateObjects[i] = getRandomEstateObject(i);
+  estateObjects.push(getRandomEstateObject(i));
   fragmentPin.appendChild(getNewPin(estateObjects[i]));
 }
 insertPlacePin.appendChild(fragmentPin);
